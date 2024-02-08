a917141caf0089e79e18c8096899fa5970b568d8
Mark Rattle <mark.rattle@gmail.com>
2024-02-08 18:46:27 -0500

    stretches

--- src/ColourSwatch.js
+++ src/ColourSwatch.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ColourSwatch.css';
 
 function ColourSwatch(props) {
   const { colour, correctColour, setIsCorrect } = props;
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <div 
-      className="ColourSwatch"
-      style={{backgroundColor: colour}}
-      onClick={() => {
-          console.log(`clicked on ${colour}`)
-          if (colour === correctColour) {
-            setIsCorrect(true);
-          } else {
-            setIsCorrect(false);
-          }
-        }}>
-    </div>
+      <div 
+        className="ColourSwatch"
+        style={{backgroundColor: colour}}
+        onClick={() => {
+            console.log(`clicked on ${colour}`)
+            if (isClicked === false) {
+              setIsClicked(true);
+            }
+            if (colour === correctColour) {
+              setIsCorrect(true);
+            } else {
+              setIsCorrect(false);
+            }
+          }}>
+        { isClicked ? <p>{colour}</p> : null }
+      </div>
   );
 }
 
